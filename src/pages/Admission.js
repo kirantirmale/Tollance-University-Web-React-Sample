3a31b0d1c227faebb6f4739c62bad613f7eceada
Kiran Tirmale <127860308+kirantirmale@users.noreply.github.com>
2024-07-21 12:04:49 +0530

    update commit

---
+++ src/pages/Admission.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Breadcrumb } from '../components/common/Breadcrumb'
+import { PopularArticles } from '../components/common/PopularArticles'
+import { Link } from 'react-router-dom'
+
+export const Admission = () => {
+    return (
+        <div>
+            <Breadcrumb />
+        </div>
+    )
+}
---
+++ src/pages/Transcript.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Breadcrumb } from '../components/common/Breadcrumb'
+import { PopularArticles } from '../components/common/PopularArticles'
+import { Link } from 'react-router-dom'
+
+export const Transcript = () => {
+    return (
+        <div>
+            <Breadcrumb />
+            <div className="container">
+                qgasguqfiuewvdi
+            </div>
+        </div>
+
+    )
+}
